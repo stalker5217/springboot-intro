678bc876d781f9a4587269f09c558aca83f725dd
Song <stalker5217@gmail.com>
2020-06-27 16:20:43 +0900

    userName 변수 이름 수정

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# springboot-intro
-springboot clone coding
+# Spring-Boot Project Clone Coding
+
+본 프로젝트는 
+'스프링 부트와 AWS로 혼자 구현하는 웹 서비스, 이동욱, 프리렉'
+의 내용의 clone coding입니다.
+
+
--- src/main/java/com/springboot/web/IndexController.java
+++ src/main/java/com/springboot/web/IndexController.java
@@ -32,7 +32,7 @@ public class IndexController {
         model.addAttribute("posts", postsService.findAllDesc());
 
         if(user != null){
-            model.addAttribute("userName", user.getName());
+            model.addAttribute("login_name", user.getName());
         }
 
         return "index";
